a0cd88a475630a7aadf5899539941d83455e3cd1
Coops023 <bjorkc27@gmail.com>
2022-03-02 17:06:21 +0100

    initial commit heroku build

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+require("dotenv").config();
+const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
 
 const cors = require("cors");
 
@@ -11,11 +13,13 @@ app.use(cors());
 require("./models/quote");
 
 mongoose
-  .connect(`mongodb://127.0.0.1:27017/deploy-mern-db`, {
+  .connect(MONGODB_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB has been connected"))
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
   .catch((err) => console.log(err));
 
 //middleware
@@ -26,6 +30,16 @@ app.use(bodyParser.json());
 require("./routes/quoteRoute.js")(app);
 
 const PORT = process.env.PORT || 5000;
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
